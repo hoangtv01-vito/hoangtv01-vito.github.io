ddd993142ddd1106e60291791f1c8c521b5352a6
hoangtv01-vito <hoang111274@gmail.com>
2024-09-15 17:52:29 +0700

    Add pseudo border for home image

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Typing Animation
+
+var typed = new Typed(".typing",{
+    strings:["", "Vulnerability Analyst", "Linux Administrator", "Windows Administrator", "Cloud Administrator"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
